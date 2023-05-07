0fc5d1784c9ed58e3fd7c64bb082dd89c97efd63
Nikky-kallaje <nkallaje447@gmail.com>
2023-05-07 16:56:57 +0530

    Handling optional params with ? symbol

--- app.js
+++ app.js
@@ -37,7 +37,9 @@ res.status(200).json({
 })
 })
 //Route for passing ID in request URL
-app.get('/api/v1/tours/:id', (req, res) =>{
+app.get('/api/v1/tours/:id/:x/:y?', (req, res) =>{
+
+  
   console.log(req.params);
   res.status(200).json({
     status:'Success',
